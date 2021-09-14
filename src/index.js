7b92518f192b44cf1755489a081727a251f8007b
Deeparkrish <kosalai17@gmail.com>
2021-09-13 18:29:21 -0700

    about component and nav

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+//The ReactDOM library is rendering the App component at the root element in the HTML
 ReactDOM.render(
   <React.StrictMode>
     <App />
